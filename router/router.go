d08ae388333f6028b0b667af2e666b54d1b3fb77
hhertout <hugues.hertout@gmail.com>
2023-12-07 19:41:16 +0100

    feat: adding cors

--- router/router.go
+++ router/router.go
@@ -1,12 +1,15 @@
 package router
 
 import (
+	"github.com/eco-challenge/config"
 	"github.com/eco-challenge/controller"
 	"github.com/gin-gonic/gin"
 )
 
 func Provider() *gin.Engine {
 	r := gin.Default()
+
+	r.Use(config.CORSMiddleware())
 
 	r.GET("/ping", controller.Ping)
 	r.GET("/download", controller.GetFile)
